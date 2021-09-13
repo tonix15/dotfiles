346c9354540b31179aade9ed4de87d03354e026d
Anthony Gerard Mondoy <tonix15@gmail.com>
2021-09-13 17:09:23 +0800

    migrate emacs config to use org babel

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,15 @@
-;;; package --- init.el config
+(require 'org-tempo)
 
-;; (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-					; (org-babel-load-file (expand-file-name "~/.dotfiles/emacs/custom.org"))
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
 
-;; User Interface
-(setq inhibit-startup-message t)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp .t)
+   (python . t)))
+
+(setq org-confirm-babel-evaluate nil)
 
 (prefer-coding-system 'utf-8-unix)
 (set-locale-environment "en_PH.UTF-8")
@@ -18,42 +23,12 @@
 (setq process-coding-system-alist
       (cons '("grep" utf-8 . utf-8) process-coding-system-alist))
 
-(scroll-bar-mode -1) ; disable visible scrollbar
-(tool-bar-mode -1)   ; disable the toolbar
-(tooltip-mode -1)    ; disable tooltips
-(set-fringe-mode 10) ; give some breathing room
-
-(menu-bar-mode -1)   ; disable menu bar
-
-(setq visible-bell t) ; set up visual bell
-
-; set font
-(set-face-attribute 'default nil :font "Fira Code")
-
-; set default theme
-(load-theme 'wombat t)
-
-;; enable line numbers
-(column-number-mode)
-(global-display-line-numbers-mode t)
-;; disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode(lambda () (display-line-numbers-mode 0))))
-
-(global-subword-mode 1)
-
-;; Initialize package sources
 (require 'package)
-
-; (setq package-enable-at-startup nil)
 
 (setq package-archives
              '(("melpa" . "https://melpa.org/packages/")
-	       ("org" . "https://orgmode.org/elpa/")
-	       ("elpa" . "https://elpa.gnu.org/packages/")))
+               ("org" . "https://orgmode.org/elpa/")
+               ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
@@ -64,31 +39,78 @@
   (package-install 'use-package))
 
 (require 'use-package)
+
 (setq use-package-always-ensure t)
 
-;; Load which-key
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)  
+(tooltip-mode -1)   
+
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+
+(set-fringe-mode 10)
+
+(set-face-attribute 'default nil :font "Fira Code")
+
+(load-theme 'wombat t)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode(lambda () (display-line-numbers-mode 0))))
+
+(use-package all-the-icons)
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package git-gutter
+  :ensure t
+  :init (global-git-gutter-mode +1))
+
+(global-subword-mode 1)
+
+(use-package hungry-delete
+  :ensure t
+  :config (global-hungry-delete-mode))
+
+(use-package simpleclip
+  :ensure t
+  :init
+  (simpleclip-mode 1))
+
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.9))
 
-;; add ivy package
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
 
@@ -99,28 +121,16 @@
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history))
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . counsel-minibuffer-history))
   :config
   (setq ivy-initial-inputs-alist nil)) ;; don't start start searches with ^
 
-;; add doom modeline
-(use-package doom-modeline
-  :init (doom-modeline-mode 1))
-
-;; add rainbow delimiter
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;; icons package
-(use-package all-the-icons)
-
-(use-package all-the-icons-dired
-  :hook (dired-mode . all-the-icons-dired-mode))
-
-;; Projectile
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
@@ -135,10 +145,15 @@
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
-;; Magit
 (use-package magit)
 
-;; LSP
+(use-package smartparens
+  :config
+  (setq sp-show-pair-from-inside nil)
+  (require 'smartparens-config)
+  (add-hook 'prog-mode-hook #'smartparens-mode)
+  :diminish smartparens-mode)
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init
@@ -160,9 +175,9 @@
   :hook (prog-mode . company-mode)
   :bind
   (:map company-active-map
-	("<tab>" . company-complete-selection))
+        ("<tab>" . company-complete-selection))
   (:map lsp-mode-map
-	("<tab>" . company-indent-or-complete-common))
+        ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0)
@@ -173,7 +188,6 @@
   :hook
   (company-mode . company-box-mode))
 
-;; Programming
 (use-package typescript-mode
   :mode "\\.ts\\'"
   :hook (typescript-mode . lsp-deferred)
@@ -193,25 +207,56 @@
   :after lsp
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-	 ("\\.md\\'" . markdown-mode)
-	 ("\\.markdown\\'" . markdown-mode))
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
   :init
   (setq markdown-command "multimarkdown"))
 
-;; Keybindings
 (use-package general)
+
 (general-define-key
  "<escape>" 'keyboard-escape-quit
  "C-M-b" 'counsel-switch-buffer
  "C-c k b" 'tonyo/kill-all-buffers
  "C-c w l" 'tonyo/copy-whole-line)
 
-;; Kill all buffers
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)))
+
+(defun tonyo/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.dotfiles/emacs/settings.org"))
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda ()
+                           (add-hook 'after-save-hook #'tonyo/org-babel-tangle-config)))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▼"
+        org-hide-emphasis-markers t)
+
+  (setq org-agenda-files
+        '("~/Documents/Work/Tasks.org")))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("⊛" "⊕" "⊗" "⊕" "⊗" "⊕" "⊗")))
+
+(font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
 (defun tonyo/kill-all-buffers()
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
-;; Copy Whole Line
 (defun tonyo/copy-whole-line()
   (interactive)
   (save-excursion
@@ -220,39 +265,9 @@
       (point-at-bol)
       (point-at-eol)))))
 
-;; Org Mode
-(defun efs/org-mode-setup ()
+(defun tonyo/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
   (auto-fill-mode 0)
   (visual-line-mode 1)
   (setq evil-auto-indent nil))
-
-(use-package org
-  :hook (org-mode . efs/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▼"
-	org-hide-emphasis-markers t))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("⊛" "⊕" "⊗" "⊕" "⊗" "⊕" "⊗")))
-
-;; replace list hyphen
-(font-lock-add-keywords 'org-mode
-			'(("^ *\\([-]\\) "
-			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-;; (dolist (face '((org-level-1 . 1.2)
-;; 		(org-level-2 . 1.1)
-;; 		(org-level-3 . 1.05)
-;; 		(org-level-4 . 1.0)
-;; 		(org-level-5 . 1.1)
-;; 		(org-level-6 . 1.1)
-;; 		(org-level-7 . 1.1)
-;; 		(org-level-8 . 1.1)))
-;;   (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-		
-		
